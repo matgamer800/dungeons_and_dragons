ee9029946ee6eed1041ff288b27555c252a7187f
MatheusGUP <92118481+matgamer800@users.noreply.github.com>
2024-09-23 00:28:37 -0300

    Update Player.kt

--- DeD/src/main/kotlin/Player/Player.kt
+++ DeD/src/main/kotlin/Player/Player.kt
@@ -1,7 +1,7 @@
 package Player
 
 import Classes.Classes
-import Raca.Racas
+import Raca.*
 
 
 class Player {
@@ -13,10 +13,10 @@ class Player {
     var Nivel:Int = 1
     var Xp:Float =0.0f
 
-    constructor(nome:String, habilidade: Habilidade,racas: Racas,classes: Classes){
+    constructor(nome:String, habilidade: Habilidade,racas: String,classes: Classes){
         this.Nome = nome
         this.Classe = classes
-        this.Raca = racas
+        this.Raca = acharRaca(racas)
         var arrayRaca: Array<Int>
         arrayRaca = this.Raca.HabilidadeRaca()
         habilidade.atualizarHabilidade(arrayRaca)
@@ -50,6 +50,32 @@ class Player {
         deve-se utilizar when para definir classes e racas
         
     */
+    private fun acharRaca(nomeRaca:String) : Racas{
+        var RacaDefinido : Racas = Humano()
+
+        when(nomeRaca){
+                "Anão" -> {RacaDefinido = Anao()}
+                "Anão da Colina" -> {RacaDefinido = Anao_Colina()}
+                "Anão da Montanha" -> {RacaDefinido = Anao_Montanha()}
+                "Elfo" -> {RacaDefinido = Elfo()}
+                "Alto Elfo" -> {RacaDefinido =Alto_Elfo()}
+                "Elfo da Floresta" -> {RacaDefinido =Elfo_Floresta()}
+                "Elfo Negro (Drow)" -> {RacaDefinido =Drow()}
+                "Meio-Elfo" -> {RacaDefinido =Meio_Elfo()}
+                "Halfling" -> {RacaDefinido = Halfling()}
+                "Halfling dos Pés Leves" -> {RacaDefinido = Helfling_Pes_Leve()}
+                "Halfling Robusto" -> {RacaDefinido =Helfling_Robusto()}
+                "Humano" -> {RacaDefinido =Humano()}
+                "Draconato" -> {RacaDefinido =Draconato()}
+                "Gnomo" -> {RacaDefinido =Gnomo()}
+                "Gnomo da Floresta" -> {RacaDefinido =Gnomo_Floresta()}
+                "Gnomo das Rochas" -> {RacaDefinido =Gnomo_Rochas()}
+                "Meio-Orc" -> {RacaDefinido = Meio_Orc()}
+                "Tieflings" -> {RacaDefinido = Tiefling()}
+        }
+
+        return RacaDefinido
+    }
 
 
 
